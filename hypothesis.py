4c84a84e7e07335b79b0e734abc2788e2683c65d
lrmantovani10 <lrmantovani10@gmail.com>
2021-12-14 18:49:19 -0600

    updating hypothesis code

--- hypothesis.py
+++ hypothesis.py
@@ -90,6 +90,7 @@ class Hypothesis(object):
         """
         labels=np.array(list(self.LABELS.keys()))
         yy=np.ones(len(labels))*((1-prob-e)/(len(labels)-1))
+        float(yy)
         yy[np.where(labels==l)[0][0]]=prob-e
         dy=pd.DataFrame(yy).ewm(alpha=.8).mean()
         dy=dy/dy.sum()
